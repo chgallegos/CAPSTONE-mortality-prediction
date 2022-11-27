e5b3a63160086d58255d17f5b1c3deffebb6feec
Alexi-Micinski <106785377+Alexi-Micinski@users.noreply.github.com>
2022-11-26 18:00:47 -0700

    mock db v2

--- Capstone_Create_Tables.sql
+++ Capstone_Create_Tables.sql
@@ -1,42 +1,7 @@
 -- Creating tables for Capstone_DB
-CREATE TABLE deaths_age_group (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
-);
-
-CREATE TABLE cod_under5 (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
-);
-
-CREATE TABLE cod_70 (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
-);
-
-CREATE TABLE cod_50_69 (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
-);
-
-CREATE TABLE cod_15_49 (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
-);
-
-CREATE TABLE cod_5_14 (
-     cod_ID INT() NOT NULL,
-     Entity VARCHAR(40) NOT NULL,
-	 Year INT() NOT NULL,
-     PRIMARY KEY (cod_ID),
+CREATE TABLE IF NOT EXISTS cause_of_death (
+	country TEXT PRIMARY KEY,
+	age_range TEXT NOT NULL,
+	number_of_deaths INT NOT NULL,
+	cause_of_death STRING NOT NULL
 );

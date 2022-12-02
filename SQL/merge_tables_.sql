ea2713d9ead6e2a100a2a78d338f40e320dcaaa2
Bren42 <nytech42@gmail.com>
2022-12-02 10:28:56 -0700

    added files for the SQL table merge, we could perform the join with the population data off of this.

---
+++ SQL/merge_tables_.sql
@@ -0,0 +1,13 @@
+Insert Into cod_under5 Select * FROM cod_15to49;
+Insert Into cod_under5 Select * FROM cod_5to14;
+Insert Into cod_under5 SELECT * FROM cod_50to69;
+Insert Into cod_under5 SELECT * FROM cod_70;
+
+
+select * 
+Into comb_age_groups
+from cod_under5;
+
+
+Select *
+from comb_age_groups;

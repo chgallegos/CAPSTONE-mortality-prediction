620a7d7bc3911a32339fef8eab728c4c79365b18
Bren42 <nytech42@gmail.com>
2022-12-17 08:40:00 -0700

    adding updated flask app and initial html template

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,14 @@
 #Basic Flask App
 
 #import dependencies
-from flask import Flask
+from flask import Flask, render_template
 
 #assign flask to a variable
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+
 #set our routes here
 @app.route('/')
-def hello_world():
-    return 'Welcome to Mortality Prediction Page...More to come'
+def index():
+    return render_template('main_page.html')
